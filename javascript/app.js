d2949c734171c16996cd262da3a1653c1c2be81c
Berni <bernalpas@gmail.com>
2021-05-21 21:38:53 -0300

    Sumamos un archivo JS

---
+++ javascript/app.js
@@ -0,0 +1,51 @@
+
+//Estos son los comentarios
+
+/* 
+1. Defino las variables.
+*/
+var numero1;
+var numero2;
+
+let suma;
+let resta;
+let division;
+let multiplicacion;
+
+const PI = 3.14216468;
+
+/* 
+2. Función: es un método o proceso que se repite
+ */
+function inicio(){
+    numero1 = prompt('Introduce el Número 1: ');
+    numero2 = prompt('Introduce otro Número: ');
+
+    console.log('El primer número es: ' + numero1);//Concatenación
+
+    console.log(`EL segundo número es: ${numero2}`);//Template
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
